1e6c3f90dfab90febfc2b76bf1a7b801c61d8b79
shreekunjvariya <shreekunjvariya@gmail.com>
2021-07-25 12:31:35 +0530

    Online URL Music Stream from Firebase Database

--- app/src/main/java/com/example/muxic_world/MainActivity.java
+++ app/src/main/java/com/example/muxic_world/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.muxic_world;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,22 +48,26 @@ public class MainActivity extends AppCompatActivity {
 
     // creating a variable for our
     // Database Reference for Firebase.
-    DatabaseReference databaseReference;
+    DatabaseReference databaseReferenceAud, databaseReferenceVid;
+    PlayerView playerView;
+    SimpleExoPlayer exoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        playerView = (PlayerView) findViewById(R.id.idExoPlayerView);
         // below line is used to get the instance
         // of our Firebase database.
         firebaseDatabase = FirebaseDatabase.getInstance("https://musicworld-be365-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
         // below line is used to get reference for our database.
-        databaseReference = firebaseDatabase.getReference("url");
+        databaseReferenceAud = firebaseDatabase.getReference(getString(R.string.audurl));
+        databaseReferenceVid = firebaseDatabase.getReference(getString(R.string.vidurl));
 
         // calling add value event listener method for getting the values from database.
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReferenceAud.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // this method is call to get the realtime updates in the data.
@@ -92,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Audio has not played", Toast.LENGTH_SHORT).show();
             }
         });
+        getVideoUrl();
     }
 
     private void playAudio(String audioUrl) {
@@ -115,6 +135,82 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             // this line of code is use to handle error while playing our audio file.
             Toast.makeText(this, "Error found is " + e, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+//Play Video in ExoVideo Player
+
+
+    private void getVideoUrl() {
+        // calling add value event listener method
+        // for getting the values from database.
+        databaseReferenceVid.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // this method is call to get the
+                // realtime updates in the data.
+                // this method is called when the
+                // data is changed in our Firebase console.
+                // below line is for getting the data
+                // from snapshot of our database.
+                String videoUrl = snapshot.getValue(String.class);
+
+                // after getting the value for our video url
+                // we are passing that value to our
+                // initialize exoplayer method to load our video
+                initializeExoplayerView(videoUrl);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // calling on cancelled method when we receive
+                // any error or we are not able to get the data.
+                Toast.makeText(MainActivity.this, "Fail to get video url.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initializeExoplayerView(String videoURL) {
+        try {
+            // bandwidth meter is used for getting default bandwidth
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            // track selector is used to navigate between video using a default seeker.
+            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+
+            // we are adding our track selector to exoplayer.
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+
+            // we are parsing a video url and
+            // parsing its video uri.
+            Uri videouri = Uri.parse(videoURL);
+
+            // we are creating a variable for data source
+            // factory and setting its user agent as 'exoplayer_view'
+            DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+
+            // we are creating a variable for extractor
+            // factory and setting it to default extractor factory.
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+            // we are creating a media source with above variables
+            // and passing our event handler as null,
+            MediaSource mediaSource = new ExtractorMediaSource(videouri, dataSourceFactory, extractorsFactory, null, null);
+
+            // inside our exoplayer view
+            // we are setting our player
+            playerView.setPlayer(exoPlayer);
+
+            // we are preparing our exoplayer
+            // with media source.
+            exoPlayer.prepare(mediaSource);
+
+            // we are setting our exoplayer
+            // when it is ready.
+            exoPlayer.setPlayWhenReady(true);
+        } catch (Exception e) {
+            // below line is used for handling our errors.
+            Log.e("TAG", "Error : " + e.toString());
         }
     }
 }
